8c29eb306c39acad0b8fa0e55e2a29c10ab094c4
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2020-06-01 00:14:03 +0300

    theme

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:firstapp/ui/home.dart';
 import 'package:firstapp/ui/scaffold.dart';
 import 'package:firstapp/ui/mycard.dart';
@@ -6,7 +5,56 @@ import 'package:firstapp/ui/info.dart';
 import 'package:firstapp/ui/tipcalculator.dart';
 import 'package:firstapp/ui/quize.dart';
 import 'package:firstapp/ui/movie/movie.dart';
+import 'package:flutter/material.dart';
 
 //void main() => runApp(ScaffoldExample());
+
+/*
+final ThemeData appTheme = buildAppTheme();
+ThemeData buildAppTheme() {
+  final ThemeData base = ThemeData.dark();
+  return base.copyWith(
+    brightness: Brightness.dark,
+    accentColor: Colors.amber,
+    primaryColor: Colors.green,
+    scaffoldBackgroundColor: Colors.red,
+    backgroundColor: Colors.amber,
+    textTheme: appTextTheme(base.textTheme),
+  );
+}
+
+TextTheme appTextTheme(TextTheme base) {
+  return base.copyWith(
+      headline: base.headline.copyWith(
+          fontWeight: FontWeight.w500
+      ),
+      title: base.title.copyWith(
+          fontSize: 18.0
+      ),
+      caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0
+      ),
+      button: base.button.copyWith(
+          fontSize: 23.9,
+          backgroundColor: Colors.red
+      ),
+      body1: base.body1.copyWith(
+          fontSize: 16.9,
+          fontFamily: 'Lobster',
+          color: Colors.white
+      )
+  ).apply(
+    fontFamily: 'Lobster',
+    displayColor: Colors.amber,
+    //bodyColor: Colors.red
+  )
+}
+void main() =>
+    runApp(new MaterialApp(
+      theme: buildAppTheme(),
+      home: MovieListView(),
+    ));
+*/
 
 void main() => runApp(new MaterialApp(home: MovieListView()));
