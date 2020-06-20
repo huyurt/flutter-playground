42eb436039e8f5646180ea4914c05aafc69b6f58
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2020-06-21 01:41:51 +0300

    todo

---
+++ secondapp/todo/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/models/task_data.dart';
+import 'package:todo/screens/task_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => TaskData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TaskScreen(),
+      ),
+    );
+  }
+}
---
+++ secondapp/todo/lib/widgets/task_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/models/task_data.dart';
+
+import 'task_tile.dart';
+
+class TaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+        itemCount: taskData.taskCount,
+        itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
+          return TaskTile(
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            checkboxCallback: (checkboxState) {
+              taskData.updateTask(task);
+            },
+            longPressCallback: () {
+              taskData.deleteTask(task);
+            },
+          );
+        },
+      );
+    });
+  }
+}
