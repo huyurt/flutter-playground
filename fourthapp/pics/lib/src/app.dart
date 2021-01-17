977194834550641f258bfd17d4eea71dadc830ae
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2021-01-17 21:56:38 +0300

    pics

---
+++ fourthapp/pics/lib/src/app.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:pics/src/models/image_model.dart';
+import 'package:pics/src/widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  var counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = new ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+  }
+}
