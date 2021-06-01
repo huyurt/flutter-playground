33a06cc430b5b0ebe672e4db7c35440d0cf9f21e
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2021-06-01 15:35:54 +0300

    no message

---
+++ flutter/getxlogin/lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.all(16),
+        child: Container(),
+      ),
+    );
+  }
+}
